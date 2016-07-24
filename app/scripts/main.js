e59c022be9ec49513718f5e326e3b43abd5a356f
Neil Pullman <npullman@thisisgrow.com>
2016-07-24 01:45:02 -0400

    switch to canvas, change interactivity to filter eq rather than adjust playrate, and fix loading on ios

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,64 +1,113 @@
 import Soundcloud from 'soundcloud-audio';
 
 
-let clientID = '8d7651d1002e11ab38b3294f6b2ffaa0',
+let width = document.documentElement.clientWidth,
+    height = document.documentElement.clientHeight,
+    clientID = '8d7651d1002e11ab38b3294f6b2ffaa0',
     soundcloud = new Soundcloud(clientID),
-    elements = [], source, analyser, context;
+    elements = [], source, biquadFilter, analyser,
+    AudioContext = AudioContext || webkitAudioContext,
+    context = new AudioContext(),
+    filter = 20000 * (1 - (width - 200) / width),
+    canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d'),
+    userHasInteracted = false,
+    audio = new Audio();
+
+audio.crossOrigin = 'anonymous';
+canvas.width = width;
+canvas.height = height;
+
+if (window.devicePixelRatio > 1) {
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
+
+  canvas.width = canvasWidth * window.devicePixelRatio;
+  canvas.height = canvasHeight * window.devicePixelRatio;
+  canvas.style.width = canvasWidth;
+  canvas.style.height = canvasHeight;
+
+  ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+}
+
+if (!('ontouchend' in window)) {
+  startAudio();
+}
 
 soundcloud.resolve('https://soundcloud.com/couldever/igotitwrong', (track) => {
-  let audio = new Audio();
-  audio.crossOrigin = 'anonymous';
   audio.src = `${track.stream_url}?client_id=${clientID}`;
-
-  context = new AudioContext();
-  analyser = context.createAnalyser();
-
-  source = context.createMediaElementSource(audio);
-  source.connect(analyser);
-  analyser.connect(context.destination);
-  console.log(source);
-
-  source.mediaElement.play();
-
-  analyser.fftSize = 4096;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
-
-  initDomElements(analyser.fftSize / 2);
-
-  let output = () => {
-    requestAnimationFrame(output);
-
-    var dataArray = new Uint8Array(bufferLength);
-    analyser.getByteFrequencyData(dataArray);
-
-    // console.log(dataArray);
-
-    for (var i = 0; i < analyser.fftSize / 2; i++) {
-      // console.log(dataArray[i] / 255)
-      elements[i].style.opacity = ((dataArray[i] / 255) * 1000 | 0) / 1000;
-    }
-  }
-
-  output();
-
-
-  document.body.addEventListener('mousemove', (e) => {
-    var halfWidth = window.innerWidth / 2;
-
-    source.mediaElement.playbackRate = ((e.pageX - halfWidth) / halfWidth + 2) / 2;
-    // source.mediaElement.playbackRate -= .1;
-  });
-
-  function initDomElements (count) {
-    var container = document.querySelector('section');
-
-    for (var i = 0; i < count; i++) {
-      var div = document.createElement('div');
-
-      container.appendChild(div);
-    }
-
-    elements = document.querySelectorAll('div');
-  }
 });
+
+var unlock = function() {
+  startAudio();
+
+  document.removeEventListener('touchend', unlock, true);
+};
+
+document.addEventListener('touchend', unlock, true);
+
+function startAudio(e) {
+  console.log('start audio')
+
+    analyser = context.createAnalyser();
+    biquadFilter = context.createBiquadFilter();
+    source = context.createMediaElementSource(audio);
+
+    source.connect(biquadFilter);
+    biquadFilter.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 32;
+    biquadFilter.type = 'bandpass';
+    biquadFilter.frequency.value = filter;
+    biquadFilter.Q.value = 0;
+
+    audio.addEventListener('canplay', (e) => {
+      console.log('canplay')
+      source.mediaElement.play();
+    })
+
+    audio.play();
+
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    var segments = analyser.fftSize / 2;
+    var lineWidth = width / segments | 0;
+
+    // initDomElements(analyser.fftSize / 2);
+
+    let output = () => {
+      requestAnimationFrame(output);
+      ctx.clearRect(0, 0, width, height);
+
+      var dataArray = new Uint8Array(bufferLength);
+      analyser.getByteFrequencyData(dataArray);
+
+      for (var i = 0; i < segments; i++) {
+        ctx.strokeStyle = `rgba(0, 0, 0, ${((dataArray[i] / 255) * 1000 | 0) / 1000})`;
+        ctx.fillStyle = `rgba(0, 0, 0, ${((dataArray[i] / 255) * 1000 | 0) / 1000})`;
+
+        ctx.beginPath();
+        ctx.lineWidth = lineWidth;
+        ctx.moveTo(i * lineWidth, 0);
+        ctx.lineTo(i * lineWidth, height);
+        ctx.stroke();
+      }
+    }
+
+    output();
+
+    let updateFilter = (e) => {
+      e.preventDefault();
+
+      if (!userHasInteracted) {
+        userHasInteracted = true;
+        biquadFilter.Q.value = 1;
+      }
+
+      biquadFilter.frequency.value = 16000 * (1 - (width - e.pageX) / width) | 0;
+    };
+
+    document.body.addEventListener('mousemove', updateFilter);
+    document.body.addEventListener('touchmove', updateFilter);
+}
